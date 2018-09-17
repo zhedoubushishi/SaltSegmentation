5d15ef79157b57609f6ca1aef47a84f32b94ce1c
Shanlin Sun <shanlins@usc.edu>
2018-09-17 00:21:33 -0700

    expension of config
    *yaml file controls config
    *version auto update
    *ready to 'test' module

--- src/config.py
+++ src/config.py
@@ -6,49 +6,75 @@ import src.utils
 # all the parameters used for project
 ################################################################################
 
+# read data
+with open('../config/config.yaml','r') as f:
+    data = yaml.load(f)
+
+DATA = data
 # file path
 if platform.system() == 'Windows':
-    TRAIN_IMG_PATH = "E:\SaltSegmentation\\raw_data\\train\\images\\"
-    TEST_IMG_PATH = "E:\SaltSegmentation\\raw_data\\test\\images\\"
-    DEPTH_PATH = "E:\SaltSegmentation\\raw_data\\depths.csv"
-    TRAIN_MASK_PATH = "E:\SaltSegmentation\\raw_data\\train\\masks\\"
-    TRAIN_INFO_PATH = "E:\SaltSegmentation\\raw_data\\train.csv"
+    TRAIN_IMG_PATH = data['windows_path']['TRAIN_IMG_PATH']
+    TEST_IMG_PATH = data['windows_path']['TRAIN_IMG_PATH']
+    DEPTH_PATH = data['windows_path']['TRAIN_IMG_PATH']
+    TRAIN_MASK_PATH = data['windows_path']['TRAIN_IMG_PATH']
+    TRAIN_INFO_PATH = data['windows_path']['TRAIN_IMG_PATH']
 else:
     pass
 
-#
-
 # basic parameters
-IMG_ORI_SIZE = 101
-IMG_TAR_SIZE = 101
+IMG_ORI_SIZE = data['basic_para']['IMG_ORI_SIZE']
+IMG_TAR_SIZE = data['basic_para']['IMG_TAR_SIZE']
 
-version = 1
+version = data['version']
+
+# Keras Model parameters
+START_NEURONS = data['model_build']['START_NEURONS']
+DROPOUT_RATIO = data['model_build']['DROPOUT_RATIO']
+
+MODEL1_ADAM_LR = data['model_train'][0]['MODEL_ADAM_LR']
+MODEL1_EPOCHS = data['model_train'][0]['MODEL_EPOCHS']
+MODEL1_BATCH_SIZE = data['model_train'][0]['MODEL_BATCH_SIZE']
+MODEL1_STEPS_PER_EPOCH_TRAIN = data['model_train'][0]['MODEL_STEPS_PER_EPOCH_TRAIN']
+MODEL1_STEPS_PER_EPOCH_VAL = data['model_train'][0]['MODEL_STEPS_PER_EPOCH_VAL']
+MODEL1_LOSS = data['model_train'][0]['MODEL_LOSS']
+
+MODEL2_ADAM_LR = data['model_train'][1]['MODEL_ADAM_LR']
+MODEL2_EPOCHS = data['model_train'][1]['MODEL_EPOCHS']
+MODEL2_BATCH_SIZE = data['model_train'][1]['MODEL_BATCH_SIZE']
+MODEL2_STEPS_PER_EPOCH_TRAIN = data['model_train'][1]['MODEL_STEPS_PER_EPOCH_TRAIN']
+MODEL2_STEPS_PER_EPOCH_VAL = data['model_train'][1]['MODEL_STEPS_PER_EPOCH_VAL']
+MODEL2_LOSS = data['model_train'][1]['MODEL_LOSS']
+
+# ReduceLROnPlateau parameters
+MODEL1_REDUCE_FACTOR = data['R_LR'][0]['MODEL_REDUCE_FACTOR']
+MODEL1_REDUCE_PATIENT = data['R_LR'][0]['MODEL_REDUCE_PATIENT']
+
+MODEL2_REDUCE_FACTOR = data['R_LR'][1]['MODEL_REDUCE_FACTOR']
+MODEL2_REDUCE_PATIENT = data['R_LR'][1]['MODEL_REDUCE_PATIENT']
+
+# mode
+FULL = 0
+TRAIN = 1
+TEST = 2
+MODE = FULL
+
+# thresholds
+THRESHOLD_BEST = data['THRESHOLD_BEST']
+
+# submission
+SUBMISSION = True
+
+# Name
 BASIC_NAME = f'Unet_resnet_v{version}'
 SAVE_MODEL_NAME = BASIC_NAME + '.model'
 SUBMISSION_NAME = BASIC_NAME + '.csv'
 
 
-# Keras Model parameters
-START_NEURONS = 16
-DROPOUT_RATIO = 0.5
+def write_new_yaml():
+    DATA['version'] = version+1
+    DATA['THRESHOLD_BEST'] = THRESHOLD_BEST
+    basic_yaml_name = f'../config/config_v{version}'
+    save_yaml_name = basic_yaml_name + '.yaml'
 
-MODEL1_ADAM_LR = 0.01
-MODEL1_EPOCHS = 100
-MODEL1_BATCH_SIZE = 64
-MODEL1_STEPS_PER_EPOCH_TRAIN = 200
-MODEL1_STEPS_PER_EPOCH_VAL = 100
-MODEL1_LOSS = "binary_crossentropy"
-
-MODEL2_ADAM_LR = 0.01
-MODEL2_EPOCHS = 50
-MODEL2_BATCH_SIZE = 64
-MODEL2_STEPS_PER_EPOCH_TRAIN = 200
-MODEL2_STEPS_PER_EPOCH_VAL = 100
-MODEL2_LOSS = "lovasz_loss"
-
-# ReduceLROnPlateau parameters
-MODEL1_REDUCE_FACTOR = 0.5
-MODEL1_REDUCE_PATIENT = 5
-
-MODEL2_REDUCE_FACTOR = 0.5
-MODEL2_REDUCE_PATIENT = 5
+    with open(save_yaml_name,'w') as f:
+        yaml.dump(DATA, f, default_flow_style=False)
